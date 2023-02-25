e9669bdaeb1f3578f7188d83c4b55611c7b3ea2c
NikolaosProgios <nikosprogios@gmail.com>
2023-02-25 11:51:51 +0200

    v1.04 - Preparation for switch between Metric and U.S. Units. - Changed app logo

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.bmicalculator
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bmicalculator.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,29 +21,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateBtn.setOnClickListener { calculateBMI() }
+        binding.apply {
+            calculateBtn.setOnClickListener { calculateBMI() }
+            swipeMetric.setOnClickListener { swipeMetric() }
+            heightEt.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+                if (event != null && event.keyCode === KeyEvent.KEYCODE_ENTER || actionId == EditorInfo.IME_ACTION_DONE) {
+                    calculateBMI()
+                }
+                false
+            })
+        }
     }
 
     private fun calculateBMI() {
         val weight = binding.weightEt.text.toString()
         val height = binding.heightEt.text.toString()
         if (validateInput(weight, height)) {
+            hideKeyboard()
             val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat() / 100))
             val bmi2Digits = String.format("%.2f", bmi).toFloat()
             displayResult(bmi2Digits)
         }
-        hideKeyboard()
     }
 
     private fun validateInput(weight: String?, height: String?): Boolean {
 
         return when {
             weight.isNullOrEmpty() -> {
-                Toast.makeText(this, "Weight is empty", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.WeightIsEmpty), Toast.LENGTH_LONG).show()
+                clearResultValues()
                 false
             }
             height.isNullOrEmpty() -> {
-                Toast.makeText(this, "Height is empty", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.HighIsEmpty), Toast.LENGTH_LONG).show()
+                clearResultValues()
                 false
             }
             else -> {
@@ -54,25 +69,27 @@ class MainActivity : AppCompatActivity() {
 
         when {
             bmi < 18.50 -> {
-                resultText = "Underweight"
+                if (bmi < 10) Toast.makeText(this, getString(R.string.CheckYourValues), Toast.LENGTH_LONG).show()
+                resultText = getString(R.string.Underweight)
                 color = R.color.under_weight
             }
             bmi in 18.50..24.99 -> {
-                resultText = "Healthy"
+                resultText = getString(R.string.Healthy)
                 color = R.color.normal
             }
             bmi in 25.00..29.99 -> {
-                resultText = "Overweight"
+                resultText = getString(R.string.Overweight)
                 color = R.color.over_weight
             }
             bmi > 29.99 -> {
-                resultText = "Obese"
+                if (bmi > 80) Toast.makeText(this, getString(R.string.CheckYourValues), Toast.LENGTH_LONG).show()
+                resultText = getString(R.string.Obese)
                 color = R.color.obese
             }
 
         }
         binding.apply {
-            infoTv.setText(R.string.normal_range_bmi)
+            infoTv.text = getString(R.string.normal_range_bmi)
             indexTv.text = bmi.toString()
             resultTv.text = resultText
             resultTv.setTextColor(resources.getColor(color))
@@ -83,6 +100,16 @@ class MainActivity : AppCompatActivity() {
         val inputMethodManager =
             getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
+    }
+
+    private fun clearResultValues() {
+        binding.apply {
+            infoTv.text=""
+            indexTv.text = ""
+            resultTv.text = ""
+        }
+    }
+    private fun swipeMetric(){
 
     }
 
